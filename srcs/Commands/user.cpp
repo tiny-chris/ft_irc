cc0a4757ad61ed0630a78af719b75c3f0cae61aa
tiny_chris <100164031+tiny-chris@users.noreply.github.com>
2023-07-02 00:54:58 +0200

    updating handleUSer() function with clearer comments

--- srcs/Commands/user.cpp
+++ srcs/Commands/user.cpp
@@ -22,25 +22,6 @@
  *              http://abcdrfc.free.fr/rfc-vf/rfc2812.html
  *              https://modern.ircdocs.horse/#connection-setup
  *
- *              1. PASS must be correct --> already a check in handleRequest
- *              2. NICK must be correct (not empty and valid - no duplicate)
- *              3. respect specific format (params):
- *                  old = USER <username> <mode> <realname> :<description>  --> version désuète (RFC 1459)
- *                  new = USER <username> <mode> <not_used> :<realname>     --> as of RFC 2812 spec for IRC
- *
- *                  with
- *                  - <username>  = un identifiant unique ou une chaîne arbitraire (pas d'espace: mais '-' ou '_')
- *                                  --> on peut imposer des restrictions sur le contenu du nom (que a-z A-Z et '-' '_')
- *                  - <mode>      = mode de connexion souhaité avec '0' comme valeur par défaut recommandée (absence de mode spécifique)
- *                                  /!\ La spécification RFC 2812 ne définit pas de modes spécifiques pour le champ <mode> de la commande "USER".
- *                                  Les modes d'utilisateur spécifiques sont généralement définis dans le contexte des canaux (channels) dans IRC,
- *                                  plutôt que dans la commande "USER". Ainsi, pour la commande "USER", il est courant d'utiliser la valeur "0"
- *                                  dans le champ <mode> pour indiquer qu'aucun mode spécifique n'est appliqué à l'utilisateur.
- *                                --> on ne va garder que ce mode par défaut
- *                  - <not_used>  = présent pour raison historique
- *                  - <realname>  = nom réel de l'utilisateur (ou tout autre nom qu'il souhaite utiliser)
- *                                   /!\ il peut y avoir des espaces si on le souhaite et dans ce cas, on utilise des quotes (' ou ")
- *                                   --> A NOUS DE VOIR CE QUE NOUS VOULONS
  */
 
 static size_t  findCharTimes(std::string str, char c)
@@ -70,10 +51,14 @@ static bool		isValidUser(std::string name)
 void		Server::handleUser( size_t cid, std::string param )
 {
   std::string	reply;
-  
+  std::string realname;
+  size_t      colon;
+
   std::cout << "client " << _clients[cid].getNickname() << " - param = <" << param << ">" << std::endl;
- 
-  // *** CHECK 1 *** 
+
+  // *** **************************** ***
+  // *** CHECK 1 - ALREADY REGISTERED ***
+  // *** **************************** ***
   // check if Client is already registered (i.e. PASS, NICK, USER are already set)
 	// if so, cannot use the USER command again
   // and send ERR_ALREADYREGISTERED numeric reply
@@ -85,15 +70,19 @@ void		Server::handleUser( size_t cid, std::string param )
     return ;
   }
 
-  // *** CHECK 2 ***
-  // check if PASS and NICK are set... otherwise, return --> create a NUMERIC REPLY ??
+  // *** **************************** ***
+  // *** CHECK 2 - PASS & NICK MUST BE SET UPSTREAM ***
+  // *** **************************** ***
+  // check if PASS and NICK are set... otherwise, return (no numeric_reply)
   if (!_clients[cid].getPassStatus() || !_clients[cid].getNickStatus())
   {
     std::cout << "client " << _clients[cid].getNickname() << " - wants to use USER command: error as PASS or NICK not set yet" << reply << std::endl;
     return ;
   }
 
-  // *** CHECK 3 *** 
+  // *** **************************** ***
+  // *** CHECK 3 - PARAM CONTENT MUST NOT BE EMPTY ***
+  // *** **************************** ***
   // check if param is not empty (min param = 1)
   // if so, cannot use the USER command and send ERR_NEEDMOREPARAMS numeric reply
   if (param.empty())
@@ -105,25 +94,15 @@ void		Server::handleUser( size_t cid, std::string param )
     return ;
   }
 
-  // *** CHECK 4 ***
-  // check each param:
-  //  - find the first ':' (after this it is only the 'realname', space is allowed inside)
-  //  - split param until ':' and send each substring into a std::vector<std::string>
-  //  - there should be 3 subtrings --> otherwise ERR_NEEDMOREPARAMS
-  //  - check on vec[0]: min 1 char, cannot be empty --> ERR_NEEDMOREPARAMS
-  //  - check on vec[1]: should be 0
-  //  - check on vec[2]: should be *
-  //  - then consider substring of param from ':' until the end
-  //      --> check content
-
-  // get the first colon (:) to split the last parameter 'realname' (after ':') from the others
-  // *** CHECK LAST PARAMETER 'realname' ***
-  // check if empty
-  //  if so --> send a ERR_NEEDMOREPARAMS reply
-  //  if not --> fill the '_realname' with content after ':'
-  size_t      colon = param.find(':', 0);
-  std::string realname;
+  // *** **************************** ***
+  // *** CHECK 4 - LAST PARAMETER 'realname' ***
+  // *** **************************** ***
+  // check if ':' exists to split the last parameter 'realname' (after ':') from the others
+  // if ':' does not exist or if realname is empty --> send a ERR_NEEDMOREPARAMS reply
+  // otherwise --> fill the '_realname' with content after ':' (space is allowed inside)
+  colon = param.find(':', 0);
   std::cout << "client " << _clients[cid].getNickname() << " - colon ':' on index <" << colon << ">" << std::endl;
+
   if (colon <= param.length())
   {
     realname = param.substr(colon + 1, param.length() - (colon + 1));
@@ -140,8 +119,12 @@ void		Server::handleUser( size_t cid, std::string param )
   _clients[cid].setRealname(realname);
   std::cout << "client " << _clients[cid].getNickname() << " - param content before ':' <" << param.substr(0, colon) << ">" << std::endl;
 
-  // *** CHECK PARAMETERS before ':' ***
-  // check number of spaces
+  // *** **************************** ***
+  // *** CHECK 5 - PARAMETERS before ':' ***
+  // *** **************************** ***
+
+  // STEP 5.1 : check number of spaces
+  // if there are not 3 spaces before ':' --> incorrect number of parameters (ERR_NEEDMOREPARAMS or just return)
   size_t nbSpaces = findCharTimes(param.substr(0, colon), ' ');
   std::cout << "client " << _clients[cid].getNickname() << " - number of spaces before ':' " << nbSpaces << std::endl;
   if ( nbSpaces != 3)
@@ -156,7 +139,7 @@ void		Server::handleUser( size_t cid, std::string param )
     return ;
   }
 
-  // get a vector of strings
+  // STEP 5.2 : split param until ':' with ' ' delimiter and send each substring into a std::vector<std::string>
   std::vector<std::string>  tokens;
   std::istringstream        iss(param.substr(0, colon));
   std::string               token;
@@ -166,12 +149,18 @@ void		Server::handleUser( size_t cid, std::string param )
     tokens.push_back(token);
   }
   std::cout << "client " << _clients[cid].getNickname() << " - number of params before ':' " << tokens.size() << std::endl;
-
   for (size_t i = 0; i < tokens.size(); ++i)
   {
-    std::cout << "client " << _clients[cid].getNickname() << " - 1st param <" << tokens[i] << ">" << std::endl; 
+    std::cout << "client " << _clients[cid].getNickname() << " - param[" << i << "] is <" << tokens[i] << ">" << std::endl;
   }
-  // check each string
+
+  // STEP 5.3 : check number of substrings and check each substring
+  // if there are less than 3 substrings or if the 1st substring has less than 1 char (cannot be empty)
+  // --> ERR_NEEDMOREPARAMS
+  // if the 1st substring contains invalid char or if there are more than 3 substrings
+  // or if 2nd substring is different from default "0" or the 3rd substring is different from default "*"
+  // --> return
+  // otherwise --> fill the '_username' with content of 1st substring
   if (tokens.size() < 3 || tokens[0].length() < 1)
   {
     reply = ERR_NEEDMOREPARAMS(_serverName, _clients[cid].getNickname(), "USER");
@@ -188,7 +177,10 @@ void		Server::handleUser( size_t cid, std::string param )
   _clients[cid].setUsername(tokens[0]);
   std::cout << "client " << _clients[cid].getNickname() << " - username is : <" << _clients[cid].getRealname() << ">" << std::endl;
 
-  if (_clients[cid].getPassStatus() == true && !_clients[cid].getNickname().empty() && !_clients[cid].getUsername().empty())
+  // *** **************************** ***
+  // *** CHECK 6 - CLIENT IS NOW REGISTERED ***
+  // *** **************************** ***
+  if (_clients[cid].getPassStatus() == true && _clients[cid].getNickStatus() == true && !_clients[cid].getUsername().empty())
   {
     _clients[cid].setIfRegistered(true);
     std::cout << "client " << _clients[cid].getNickname() << " --> is registered" << std::endl;
